b8b49a6105961582219845416969ef642ddefc4f
JosueDurand <josue.durand8@gmail.com>
2019-03-07 22:22:21 -0500

    primero

---
+++ app/src/main/java/com/example/chatheads/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.chatheads;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Check if the application has draw over other apps permission or not?
+        //This permission is by default available for API<23. But for API > 23
+        //you have to ask for the permission in runtime.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Toast.makeText(this,
+                    "Draw over other app permission not available. Closing the application",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startService(new Intent(MainActivity.this, ChatHeadService.class));
+                    finish();
+                }
+            });
+        }
+    }
+}
